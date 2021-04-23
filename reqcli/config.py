a7087d759493a32838d6a87eeadb43c8eb317faf
shiftinv <me@shiftinv.cc>
2021-04-23 17:56:47 +0200

    Don't create instance of global configuration

--- reqcli/config.py
+++ reqcli/config.py
@@ -1,16 +1,14 @@
-from dataclasses import dataclass
 from typing import Type
 from requests_cache.backends import BackendSpecifier
 
-from .type import TypeLoadConfig
+from .type.config import TypeLoadConfig
 
 
-@dataclass
-class _Configuration:
+class Configuration:
     cache_backend: BackendSpecifier = 'sqlite'
     cache_name: str = './cache/requests.db'
     default_user_agent: str = ''
     type_load_config_type: Type[TypeLoadConfig] = TypeLoadConfig
 
-
-Configuration = _Configuration()
+    def __init__(self):
+        raise AssertionError
