dac170507b4c7f82666f64ce85577fef5bda3f40
shiftinv <me@shiftinv.cc>
2021-04-26 13:52:14 +0200

    Try to make BaseSource thread-safe

--- reqcli/source/basesource.py
+++ reqcli/source/basesource.py
@@ -2,7 +2,6 @@ import urllib3
 import logging
 import requests
 import contextlib
-import requests_cache
 from requests.adapters import HTTPAdapter
 from requests_toolbelt.adapters.fingerprint import FingerprintAdapter
 from typing import Iterator, TypeVar, Union, Optional, overload
@@ -19,12 +18,12 @@ from ..type import BaseTypeLoadable
 
 _TBaseTypeLoadable = TypeVar('_TBaseTypeLoadable', bound=BaseTypeLoadable)
 
+_cache_disabled_hook = 'get_cache_disabled'
+
 _logger = logging.getLogger(__name__)
 
 
 class BaseSource:
-    # note: this class is not thread-safe
-
     def __init__(self, base_reqdata: ReqData, config: Optional[SourceConfig], *, verify_tls: bool = True, require_fingerprint: Optional[str] = None):
         # use supplied config or default
         if config is None:
@@ -45,15 +44,32 @@ class BaseSource:
 
         self._session: Union[requests.Session, CachedRateLimitedSession]
         if self._config.enable_cache:
+            # this is a hack for disabling the cache for specific requests; adding custom data to `PreparedRequest`
+            #  objects through .get/.request is surprisingly difficult (unless I'm missing something very obvious).
+            # this could probably be solved with more subclassing and overriding methods, but it would likely be more complex
+
+            # add new key to list of hooks
+            if _cache_disabled_hook not in requests.hooks.HOOKS:
+                requests.hooks.HOOKS.append(_cache_disabled_hook)
+
+            # returns false if request should not be cached
+            def filter_fn(r: Union[requests.PreparedRequest, requests.Response]) -> bool:
+                if isinstance(r, requests.PreparedRequest):
+                    return not requests.hooks.dispatch_hook(_cache_disabled_hook, r.hooks, r)
+                return True
+
+            # create cached session
             self._session = CachedRateLimitedSession(
                 cache_name=Configuration.cache_name,
                 backend=Configuration.cache_backend,
                 allowable_codes=set(self._config.cache_response_codes),
+                filter_fn=filter_fn,
                 include_get_headers=True,
                 fast_save=True,
                 requests_per_second=self._config.requests_per_second
             )
         else:
+            # create non-cached session
             self._session = RateLimitedSession(
                 requests_per_second=self._config.requests_per_second
             )
@@ -108,25 +124,18 @@ class BaseSource:
     def __get_internal(self, reqdata: ReqData, skip_cache: bool) -> requests.Response:
         reqdata = self._base_reqdata + reqdata
 
-        if skip_cache and isinstance(self._session, requests_cache.CacheMixin):
-            # not thread-safe
-            disable_cache_ctx = self._session.cache_disabled()
-            cache_disabled = True
-        else:
-            disable_cache_ctx = contextlib.nullcontext()
-            cache_disabled = False
+        _logger.debug(f'Sending request {reqdata}' + (' [cache disabled]' if skip_cache else ''))
 
-        _logger.debug(f'Sending request {reqdata}' + (' [cache disabled]' if cache_disabled else ''))
-
-        with disable_cache_ctx:
-            res = self._session.get(
-                url=reqdata.path,
-                headers=reqdata.headers,
-                params=reqdata.params,
-                cert=reqdata.cert,
-                stream=True,
-                allow_redirects=False
-            )
+        res = self._session.get(
+            url=reqdata.path,
+            headers=reqdata.headers,
+            params=reqdata.params,
+            cert=reqdata.cert,
+            stream=True,
+            allow_redirects=False,
+            # used by `filter_fn` for bypassing the cache (see `__init__` above)
+            hooks={_cache_disabled_hook: lambda r: skip_cache}
+        )
 
         if getattr(res, 'from_cache', False):
             _logger.debug(f'Got cached response for request to {reqdata.path}')
